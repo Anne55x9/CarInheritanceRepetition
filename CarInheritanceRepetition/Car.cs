b6005798c478b8cd2d12352cb34331bb6207206a
anne55x9 <anne55x9@edu.easj.dk>
2017-03-16 10:48:35 +0100

    Implemented 2 methods in the car class and overloade a constructor with 5 parameters matching the 5 properties of the class car.

--- CarInheritanceRepetition/Car.cs
+++ CarInheritanceRepetition/Car.cs
@@ -8,11 +8,66 @@ namespace CarInheritanceRepetition
 {
     public class Car
     {
+        /// <summary>
+        /// Properties of the public class car.
+        /// </summary>
+        /// 
+        public string Brand { get; set; }
         public int PriceOfCar { get; set; }
         public int YearOfPurchase { get; set; }
-        public string Brand { get; set; }
         public string RegistrationNo { get; set; }
         public int KmLiter { get; set; }
+
+        public int RegPay { get; set; }
+
+        /// <summary>
+        /// Included an overloaded constructor with 5 parameters of the public class car's properties.
+        /// </summary>
+
+        public Car(string brand, int priceOfCar, int yearOfPurchase, string registrationNo, int kmLiter)
+        {
+
+        }
+        
+        /// <summary>
+        /// Public metode RegstrationPayment that returns an int.
+        /// </summary>
+        /// 
+
+        public int RegistrationPayment()
+        {
+            if (YearOfPurchase <= 2014)
+            {
+                if (PriceOfCar <= 80500)
+                {
+                    RegPay = PriceOfCar * (105 / 100);
+                }
+                else
+                {
+                    RegPay = (80500 * (105 / 100)) + ((PriceOfCar - 80500) * (180/100));
+                }
+            }
+            else
+                if (PriceOfCar <= 81700)
+                {
+                RegPay = PriceOfCar * (105 / 100);
+                }
+                else
+                {
+                    RegPay = (81700 * (105 / 100)) + ((PriceOfCar - 81700) * (180 / 100));
+                }
+           return RegPay;
+        }
+
+        /// <summary>
+        /// Public metode SemiAnnualTax that returns an int.
+        /// </summary>
+        /// 
+
+        public int SemiAnnualTax()
+        {
+            return 1000;
+        }
 
     }
 }
