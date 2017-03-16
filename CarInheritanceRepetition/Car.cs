d86940b3e15320badb721cb704ab0449eda266b6
anne55x9 <anne55x9@edu.easj.dk>
2017-03-16 10:26:46 +0100

    Implemeneted a public class Car and included 5 properties in the class with datatypes int and string.

---
+++ CarInheritanceRepetition/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInheritanceRepetition
+{
+    public class Car
+    {
+        public int PriceOfCar { get; set; }
+        public int YearOfPurchase { get; set; }
+        public string Brand { get; set; }
+        public string RegistrationNo { get; set; }
+        public int KmLiter { get; set; }
+
+    }
+}
